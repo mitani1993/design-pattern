9dac999a7658b5ad2e256e93c3acce46fe60fba6
mitani1993 <fytim55@gmail.com>
2023-11-07 11:02:16 +0900

    TemplateMethod

---
+++ src/TemplateMethod/sample1/AbstractDisplay.java
@@ -0,0 +1,17 @@
+package TemplateMethod.sample1;
+
+public abstract class AbstractDisplay {
+    // open, print, closeはサブクラスに実装をまかせる抽象メソッド
+    public abstract void open();
+    public abstract void print();
+    public abstract void close();
+
+    // displayはAbstractDisplayで実装してるメソッド
+    public final void display() {
+        open();
+        for (int i = 0; i < 5; i++) {
+            print();
+        }
+        close();
+    }
+}
