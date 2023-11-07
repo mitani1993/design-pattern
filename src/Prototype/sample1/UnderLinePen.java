cb0f3e6b7cbcb1bdd6d7557a1736a327a5ffb538
mitani1993 <fytim55@gmail.com>
2023-11-07 12:16:27 +0900

    Prototype sample1

---
+++ src/Prototype/sample1/UnderLinePen.java
@@ -0,0 +1,32 @@
+package Prototype.sample1;
+
+import Prototype.sample1.framework.Product;
+
+public class UnderLinePen implements Product {
+    private char ulchar;
+
+    public UnderLinePen(char ulchar) {
+        this.ulchar = ulchar;
+    }
+
+    @Override
+    public void use(String s) {
+        int ulen = s.length();
+        System.out.println(s);
+        for (int i = 0; i < ulen; i++) {
+            System.out.print(ulchar);
+        }
+        System.out.println();
+    }
+
+    @Override
+    public Product createCopy() {
+        Product p = null;
+        try {
+            p = (Product)clone();
+        } catch (CloneNotSupportedException e) {
+            e.printStackTrace();
+        }
+        return p;
+    }
+}
