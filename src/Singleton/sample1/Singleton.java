db4e553680ae43174374dfd963d171276db72ace
mitani1993 <fytim55@gmail.com>
2023-11-07 11:31:41 +0900

    Singleton

---
+++ src/Singleton/sample1/Singleton.java
@@ -0,0 +1,13 @@
+package Singleton.sample1;
+
+public class Singleton {
+    private static Singleton singleton = new Singleton();
+
+    private Singleton() {
+        System.out.println("インスタンスを生成しました。");
+    }
+
+    public static Singleton getInstance() {
+        return singleton;
+    }
+}
