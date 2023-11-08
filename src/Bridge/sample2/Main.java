6083ee7f4d0df9ef6520c97b4d1b558bfc56673f
mitani1993 <fytim55@gmail.com>
2023-11-08 20:29:04 +0900

    Bridge sample2

---
+++ src/Bridge/sample2/Main.java
@@ -0,0 +1,8 @@
+package Bridge.sample2;
+
+public class Main {
+    public static void main(String[] args) {
+        RandomCountDisplay d = new RandomCountDisplay(new StringDisplayImpl("Hello, Japan!!!"));
+        d.randomDisplay(10);
+    }
+}
