03a128155be5977e24bb4873c28f251c6a4bf6d4
mitani1993 <69582233+mitani1993@users.noreply.github.com>
2023-11-13 20:13:49 +0900

    Strategy sample3

---
+++ src/Strategy/sample3/Main.java
@@ -0,0 +1,25 @@
+package Strategy.sample3;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        List<String> list = Arrays.asList("D", "B", "C", "E", "A");
+
+        // 辞書式順序で小さい順
+        list.sort(new Comparator<String>() {
+            public int compare(String a, String b) {
+                return a.compareTo(b);
+            }
+        });
+        System.out.println(list);
+
+        // 辞書式順序で大きい順
+        list.sort(new Comparator<String>() {
+            public int compare(String a, String b) {
+                return b.compareTo(a);
+            }
+        });
+        System.out.println(list);
+    }
+}
